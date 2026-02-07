293e9333135addfca739525072bf127ddb742e1b
Nidheesh018 <c.nidheeshreddy@gmail.com>
2026-02-07 14:00:25 +0530

    Add files via upload

---
+++ PHONE.java
@@ -0,0 +1,24 @@
+import java.util.*;
+public class PHONE {
+private String name;
+private String catog;
+private String email;
+private long phone_no;
+
+public PHONE(String name,String catog,String email,long phone_no){
+this.name=name;
+this.catog=catog;
+this.email=email;
+this.phone_no=phone_no;
+}
+public class main{
+    Scanner sc=new Scanner(System.in);
+    int x=sc.nextInt();
+    switch(x){
+        case 1:
+        
+
+    }
+}
+    
+}
---
+++ main.java
@@ -0,0 +1,8 @@
+import java.util.*;
+public class main{
+    Scanner sc=new Scanner(System.in);
+    Scanner sc=new Scanner(System.in);
+    System.out.println("---------Phone Book--------------");
+
+
+}
